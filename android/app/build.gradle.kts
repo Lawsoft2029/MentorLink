fd0760fe3b32d19a2354c659e69115d7eaba69fe
Your Name <your_email@example.com>
2026-09-01 16:28:18 +0100

    Configure Android manifest AdMob and user Firestore setup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,6 +15,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true // Required for flutter_local_notifications
     }
 
     kotlinOptions {
@@ -44,4 +45,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
